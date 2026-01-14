e8f13daca2b515c2e4560d6df51c814fcd04dbc2
chuhonastov <chuhonastov@yandex.ru>
2026-01-14 22:55:17 +0300

    Update routes.ts

--- webapp/src/app/routes.ts
+++ webapp/src/app/routes.ts
@@ -3,8 +3,8 @@ export const routes = {
   diagnoses: '/diagnoses',
   diagnosis: (id: string) => `/diagnoses/${id}`,
   diagnosisGroup: (id: string) => `/diagnoses/group/${id}`,
-medications: '/medications',
-medicationGroup: (id: string) => `/medications/group/${id}`,
-medication: (id: string) => `/medications/${id}`,
+  medications: '/medications',
+  medicationGroup: (id: string) => `/medications/group/${id}`,
+  medication: (id: string) => `/medications/${id}`,
   visit: '/visit'
 } as const;
